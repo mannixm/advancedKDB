511edbb8ae1f8aa131f717ce62e0c83de6146679
mannixm <mcalister.mannix@gmail.com>
2022-05-18 01:06:25 +0100

    Update CSVReader.py

--- question3/pythonAPI/CSVReader.py
+++ question3/pythonAPI/CSVReader.py
@@ -4,12 +4,10 @@ from qpython import qconnection
 from qpython.qcollection import qlist
 from qpython.qtype import QException, QTIME_LIST, QSYMBOL_LIST, QFLOAT_LIST, QLONG_LIST
 
-q=qconnection.QConnection(host='kxtraining', port=6000)
-file=open("C:/Users/mmcalister/Downloads/trade.csv", "r")
-reader = csv.reader(file, delimiter=",")
-for line in enumerate(reader):
-    time=line[1][0]
-    sym=line[1][1]
-    price=float(line[1][2])
-    size=int(line[1][3])
-    q.sendSync(('.u.upd[`trade;(`%;%;%)]')) % (sym,price,size)
+with qconnection.QConnection(host='kxtraining', port=6000) as q:
+    with open("C:/Users/mmcalister/Downloads/trade.csv", "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        for line in enumerate(reader):
+            if (line[1][0] == 'time'):
+                continue
+            q.sendSync(('.u.upd[`trade;(`{sym};{price};{size})]').format(sym=line[1][1],price=float(line[1][2]),size=int(line[1][3])))
